544ed73e4691d6d9a41a5004004e41c1154ad7f8
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-09-28 20:51:46 +0530

    simple message event

---
+++ events/message.js
@@ -0,0 +1,24 @@
+const { prefix } = require('../config.json')
+
+module.exports = async(client, message) => {
+
+if(!message.guild || message.author.bot) return;
+
+if(!message.content.toLowerCase().startsWith(prefix)) return
+if(message.content === prefix) return
+ 
+  let args = message.content.slice(prefix.length).trim().split(/ +/g);
+  let cmd = args.shift().toLowerCase();
+
+  if (message.channel.type === 'dm') return false;
+
+let command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+
+if(command) command.run(client, message, args).catch(console.log);
+if(!command) return
+  client.capitalize = string => {
+return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+
+}
+
+}
