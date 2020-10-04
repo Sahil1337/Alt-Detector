b3617b1cd40855907f2c1f49a42a44804e05c8e0
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-10-04 16:09:40 +0530

    😊Removed mongo url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "Insert your discord bot token here",
-    prefix: "a!", 
-    COOLDOWN: "10",//Must be a number
-    mongoURL: "Mongo URL",
-    owner: []//Add your ids like ["123456789", "123456789"] So you can able to use eval command!
+    token: " ",//ur bot token
+    prefix: " ",//ur bot prefix 
+    COOLDOWN: " ",//cooldown for autosetup command must be a number
+    owner: ['']//Add your ids like ["123456789", "123456789"] So you can able to use eval command!
 }
+
