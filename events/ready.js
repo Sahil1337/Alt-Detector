2575ec82d2449d361dd945d586b9286f2eff0bf6
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-10-04 16:10:17 +0530

    👌All Those Changes

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,7 @@
 const { prefix } = require('../config')
+const chalk = require('chalk')
 
 module.exports = async(client) => {
     await client.user.setActivity(`${prefix}help` , {type: 'STREAMING'}).catch(console.error);
-    console.log("Logged in to discord as "+client.user.username)
+    console.log(chalk.cyanBright("Logged in to discord as" + " " + client.user.username))
 }
