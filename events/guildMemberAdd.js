281b3a119363801779ce4e95bdd9c2af3410808a
SAHIL <69236315+HELLMAKER0001@users.noreply.github.com>
2020-10-07 11:24:56 +0530

    🤞 Some Changes

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,13 +7,13 @@ module.exports = async (client, member) => {
   let LoggingChannel = await db.get(`LoggingChannel_${member.guild.id}`);
 
   //if no channel found in the specific guild
-  if (!LoggingChannel)console.log(`Setup Is Not Done in ${member.guild.id} aka ${member.guild.name} (channel not found)`);
+  if (!LoggingChannel)console.log(`Setup Is Not Done in ${member.guild.id} aka ${member.guild.name} Guild (channel not found)`);
 
   //getting notify role
   let notifyRole = await db.get(`notifyRole_${member.guild.id}`);
 
   //if no role found in the specific guild
-  if (!notifyRole)return console.log(`Setup Is Not Done in ${member.guild.id} (${member.guild.name}) (role not found)`);
+  if (!notifyRole)return console.log(`Setup Is Not Done in ${member.guild.id} aka ${member.guild.name} Guild (role not found)`);
 
   //to get created date in days format
   let x = Date.now() - member.user.createdAt;
@@ -39,17 +39,21 @@ module.exports = async (client, member) => {
   if (created < AltAge) {
     //embed
     let altEmbed = //main alt message
-    new Discord.MessageEmbed().setTitle("Alt Found!").setColor("RANDOM")
+    new Discord.MessageEmbed().setTitle("Alt Found!").setColor("RANDOM").setFooter("Bot Made By ItzCutePihcu#0001")
       .setDescription(`
 **__Alt Name__**: ${member.user} (${member.user.username})
+
 **__ID__**: ${member.user.id}
+
 **__Account Created__**: ${created} days ago
+
 **__Account Creation Date__**: ${creationDate}
+
 **__Join Position__**: ${joinPosition}
+
 **__Join Date__**: ${joiningDate}
 `);
 
-    member.guild.channels.cache.get(LoggingChannel).send(`Notification: <@&${notifyRole}>`, altEmbed);
-    console.log(`message sent`)
+    member.guild.channels.cache.get(LoggingChannel).send(`__Notification:__ <@&${notifyRole}>`, altEmbed);
   }
 };
